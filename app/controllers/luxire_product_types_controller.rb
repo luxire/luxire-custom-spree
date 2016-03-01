79427d7f4b4d2ece6c538144e45d3199b6ba468c
Manish <manish@azureiken.com>
2016-03-01 14:21:33 +0530

    Added a params wrapper

--- app/controllers/luxire_product_types_controller.rb
+++ app/controllers/luxire_product_types_controller.rb
@@ -1,4 +1,7 @@
 class LuxireProductTypesController < Spree::Api::BaseController
+  wrap_parameters format: [:json, :xml, :url_encoded_form, :multipart_form]
+  wrap_parameters :luxire_product_type, include: [:product_type, :description, :measurement_type_ids, :image]
+
   before_action :set_luxire_product_type, only: [:show, :edit, :update, :destroy]
   require 'ostruct'
 
@@ -29,7 +32,7 @@ class LuxireProductTypesController < Spree::Api::BaseController
     @luxire_product_type_attributes_measuement_std = @luxire_product_type_attributes.where(sub_category: "std")
     @luxire_product_type_attributes_measuement_body = @luxire_product_type_attributes.where(sub_category: "body")
 
-    @luxire_custom = { id: @luxire_product_type.id,product_type: @luxire_product_type.product_type, description: @luxire_product_type.description, measurement_types: @luxire_product_type_attributes, luxire_product_attributes_ids: @luxire_product_type_attributes_ids,luxire_product_attributes: {customization_attributes: @luxire_product_type_attributes_customize, personalization_attributes:  @luxire_product_type_attributes_personalize,standard_measurement_attributes: @luxire_product_type_attributes_measuement_std, body_measurement_attributes: @luxire_product_type_attributes_measuement_body}}
+    @luxire_custom = { id: @luxire_product_type.id,product_type: @luxire_product_type.product_type, description: @luxire_product_type.description, image: @luxire_product_type.image, measurement_types: @luxire_product_type_attributes, luxire_product_attributes_ids: @luxire_product_type_attributes_ids,luxire_product_attributes: {customization_attributes: @luxire_product_type_attributes_customize, personalization_attributes:  @luxire_product_type_attributes_personalize,standard_measurement_attributes: @luxire_product_type_attributes_measuement_std, body_measurement_attributes: @luxire_product_type_attributes_measuement_body}}
 
     render json: @luxire_custom.to_json
   end
@@ -47,8 +50,11 @@ class LuxireProductTypesController < Spree::Api::BaseController
   # POST /luxire_product_types.json
   def create
     @luxire_product_type = LuxireProductType.new(luxire_product_type_params)
-    if(params[:measurement_type_ids].length>0)
-      @luxire_product_type.measurement_type_ids = params[:measurement_type_ids]
+
+    if(params[:luxire_product_type][:measurement_type_ids])
+      if(params[:luxire_product_type][:measurement_type_ids].length>0)
+        @luxire_product_type.measurement_type_ids = params[:measurement_type_ids]
+      end
     end
 
     respond_to do |format|
@@ -65,8 +71,10 @@ class LuxireProductTypesController < Spree::Api::BaseController
   # PATCH/PUT /luxire_product_types/1
   # PATCH/PUT /luxire_product_types/1.json
   def update
-    if(params[:measurement_type_ids].length>0)
-      @luxire_product_type.measurement_type_ids = params[:measurement_type_ids]
+    if(params[:luxire_product_type][:measurement_type_ids])
+      if(params[:luxire_product_type][:measurement_type_ids].length>0)
+        @luxire_product_type.measurement_type_ids = params[:measurement_type_ids]
+      end
     end
     respond_to do |format|
       if @luxire_product_type.update(luxire_product_type_params)
@@ -97,6 +105,6 @@ class LuxireProductTypesController < Spree::Api::BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def luxire_product_type_params
-      params.require(:luxire_product_type).permit!
+      params.require(:luxire_product_type).permit(:product_type, :description, :measurement_type_ids, :image)
     end
 end
