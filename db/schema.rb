2bf1775eeafaf4f4d5001b0eb383c1acb011e773
Manish <manish@azureiken.com>
2016-03-01 16:57:37 +0530

    upgraded spring and concurrent-ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226081035) do
+ActiveRecord::Schema.define(version: 20160229094830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,8 +89,12 @@ ActiveRecord::Schema.define(version: 20160226081035) do
   create_table "luxire_product_types", force: :cascade do |t|
     t.string   "product_type"
     t.string   "description"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "luxire_products", force: :cascade do |t|
@@ -175,6 +179,10 @@ ActiveRecord::Schema.define(version: 20160226081035) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "luxire_product_type_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "luxire_vendor_masters", force: :cascade do |t|
@@ -194,10 +202,14 @@ ActiveRecord::Schema.define(version: 20160226081035) do
     t.string   "name"
     t.json     "value"
     t.string   "description"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "category"
     t.string   "sub_category"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "product_measurement_types", force: :cascade do |t|
