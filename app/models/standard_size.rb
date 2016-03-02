778999b0600fbad9f5b06ed4a2600d1f54dfc328
Manish <manish@azureiken.com>
2016-03-02 20:09:48 +0530

    Added an association with LuxireProductType, LuxireProduct and Product

--- app/models/standard_size.rb
+++ app/models/standard_size.rb
@@ -1,3 +1,5 @@
 class StandardSize < ActiveRecord::Base
   belongs_to :luxire_product_type, class_name: "LuxireProductType"
+  has_many :luxire_products, class_name: 'LuxireProduct', through: :luxire_product_type
+  has_many :products, class_name: 'Spree::Product', through: :luxire_products
 end
