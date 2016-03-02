1ccc6e2147336ecab33e8b29740796c35e4ba2ae
Manish <manish@azureiken.com>
2016-03-03 00:37:26 +0530

    removed luxire_product_type_id from luxire_properties

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302142422) do
+ActiveRecord::Schema.define(version: 20160302183522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,9 +178,8 @@ ActiveRecord::Schema.define(version: 20160302142422) do
   create_table "luxire_properties", force: :cascade do |t|
     t.string   "name"
     t.string   "value"
-    t.integer  "luxire_product_type_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "luxire_stocks", force: :cascade do |t|
