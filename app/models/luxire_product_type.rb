0bd2bc304f4d88796717ce4fe52f94618fb856a0
Manish <manish@azureiken.com>
2016-03-03 01:02:11 +0530

    added dependent: destroy option for one to one or one to many relationship

--- app/models/luxire_product_type.rb
+++ app/models/luxire_product_type.rb
@@ -1,13 +1,13 @@
 class LuxireProductType < ActiveRecord::Base
   has_many :luxire_products, class_name: 'LuxireProduct'
   #has_many :luxire_product_type_style_masters, class_name: 'LuxireProductTypeStyleMaster'
-  has_many :luxire_style_masters, class_name: "LuxireStyleMaster"
+  has_many :luxire_style_masters, class_name: "LuxireStyleMaster", :dependent => :destroy
 #  has_many :luxire_style_masters, class_name: "LuxireStyleMaster" , through: :luxire_product_type_style_masters
   has_many :products, class_name: 'Spree::Product', through: :luxire_products
   has_many :product_measurement_types, class_name: "ProductMeasurementType"
   has_many :measurement_types, through: :product_measurement_types
-  has_many :standard_sizes, class_name: "StandardSize"
-  
+  has_many :standard_sizes, class_name: "StandardSize", :dependent => :destroy
+
 
   has_attached_file :image,  styles: { small: "64x64" },
                           default_style: :small,
