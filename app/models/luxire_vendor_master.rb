5c598fd23e7b07ac6851921476d830b29fdd6513
Manish <manish@azureiken.com>
2016-03-03 13:02:24 +0530

    Added presence and uniqueness validtions for name

--- app/models/luxire_vendor_master.rb
+++ app/models/luxire_vendor_master.rb
@@ -1,4 +1,6 @@
 class LuxireVendorMaster < ActiveRecord::Base
   has_many :luxire_products, class_name: "LuxireProduct"
   has_many :products, class_name: 'Spree::Product', through: :luxire_products
+
+  validates :name, presence: true, uniqueness: true
 end
