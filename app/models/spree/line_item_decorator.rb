c0f875cfcd8560178677941fec5091d013d62781
Manish <manish@azureiken.com>
2016-03-14 17:19:02 +0530

    commented accepts_nested_attribute_for :luxire_line_item

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,4 +1,4 @@
 Spree::LineItem.class_eval do
 has_one :luxire_line_item, class_name: 'LuxireLineItem'
-accepts_nested_attributes_for :luxire_line_item
+# accepts_nested_attributes_for :luxire_line_item
 end
