aaf9ec40e31fc1820274d25abf821ad6d56dc456
Manish <manish@azureiken.com>
2016-03-14 16:35:05 +0530

    Sending the last order for the customer instead of first order

--- app/controllers/custom_orders_controller.rb
+++ app/controllers/custom_orders_controller.rb
@@ -3,7 +3,7 @@ class CustomOrdersController < ApplicationController
   def get_order
       guest_token = cookies.signed[:guest_token]
       orders = Spree::Order.where(guest_token: guest_token).where(completed_at: nil)
-      render json: orders.first.to_json, status: "200"
+      render json: orders.last.to_json, status: "200"
   end
 
 end
