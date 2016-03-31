5b4c2c5deba76a20a62bce5d45cc9fd630e1f8ed
Manish <manish@azureiken.com>
2016-03-31 12:27:47 +0530

    Corrected the association with LuxireLineItem

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
 has_one :luxire_order, class_name: "LuxireOrder"
-has_many :luxire_line_item, class_name: 'LuxireLineItem', through: :spree_line_items
+has_many :luxire_line_items, class_name: 'LuxireLineItem', through: :line_items
 accepts_nested_attributes_for :luxire_order
 end
