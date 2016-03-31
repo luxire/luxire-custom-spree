50b0210a556b220a073792c9b5461aa87266112e
Manish <manish@azureiken.com>
2016-03-31 12:25:20 +0530

    Added a variable which the holds the size of the image

--- app/models/custom_image.rb
+++ app/models/custom_image.rb
@@ -1,5 +1,8 @@
 class CustomImage < ActiveRecord::Base
-  has_attached_file :image,  styles: { small: "64x64" },
+  attr_accessor :size
+
+  has_attached_file :image,
+  :styles => lambda { |a|  { :small => a.instance.size } },
                           default_style: :small,
                           url: '/luxire/custom_images/:id/:style/:basename.:extension',
                           path: ':rails_root/public/luxire/custom_images/:id/:style/:basename.:extension',
