4d0455128b33041231824fc733a9f38d0735218b
Manish <manish@azureiken.com>
2016-04-01 17:28:14 +0530

    Added a condition for the validation

--- app/models/luxire_product.rb
+++ app/models/luxire_product.rb
@@ -6,8 +6,9 @@ class LuxireProduct < ActiveRecord::Base
 
   belongs_to :luxire_stock, class_name: "LuxireStock"
   has_many :standard_sizes, class_name: "StandardSize", through: :luxire_product_type
+  attr_accessor :req_validation
 
   accepts_nested_attributes_for :luxire_stock
-  validates_presence_of (LuxireProduct.column_names - ["id","created_at","updated_at", "deleted_at", "gift_card_flag"])
-  validates_inclusion_of :gift_card_flag, in: [true, false]
+  validates_presence_of (LuxireProduct.column_names - ["id","created_at","updated_at", "deleted_at", "gift_card_flag"]), :if => lambda { |product_type| product_type.req_validation.eql? true }
+  validates_inclusion_of :gift_card_flag, in: [true, false], :if => lambda { |product_type| product_type.req_validation.eql? true }
 end
