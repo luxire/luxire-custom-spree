e86b368d623182be2f205d35a0b3682308c28260
Manish <manish@azureiken.com>
2016-04-01 16:50:30 +0530

    returning a message No in complete order exist if an in complete order does not exist

--- app/controllers/custom_orders_controller.rb
+++ app/controllers/custom_orders_controller.rb
@@ -4,7 +4,12 @@ class CustomOrdersController < Spree::Api::BaseController
       guest_token = cookies.signed[:guest_token]
       @order = Spree::Order.where(guest_token: guest_token).where(completed_at: nil).last
       # render json: orders.to_json, status: "200"
+      if @order.nil?
+        response = {msg: "No in complete order exist"}
+        render json: response.to_json, status: "404"
+      else
        render "spree/api/orders/show.v1.rabl"
+     end
   end
 
 end
