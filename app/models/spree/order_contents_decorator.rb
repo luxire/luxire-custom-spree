c06ab7ff364a455921a203fcd345654a69c1ffea
Manish <manish@azureiken.com>
2016-05-20 19:44:24 +0530

    Capturing Personalization cost if any

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -8,6 +8,7 @@ Spree::OrderContents.class_eval do
       end
 
   def add_to_line_item(variant, quantity, options = {}, luxire_line_item)
+    # byebug
      line_item = grab_line_item_by_variant(variant, false, options)
 
     #  if line_item
@@ -23,14 +24,40 @@ Spree::OrderContents.class_eval do
      line_item.target_shipment = options[:shipment] if options.has_key? :shipment
     #  Creating a transaction for line item and luxire line item
      Spree::LineItem.transaction do
+      # byebug
        line_item.save!
        unless luxire_line_item.empty?
-          #  Created LuxireLineItem
+          #  Create LuxireLineItem
            lux_line_item = LuxireLineItem.create!(luxire_line_item)
           #  Created the association
            line_item.luxire_line_item = lux_line_item
            line_item.save!
-         end
+          #  If the line item contains personalization_cost put it in the PersonalizationCost model
+          # As well as Adjustment model and update the order
+          # byebug
+           unless luxire_line_item[:total_personalization_cost] == 0
+             personalization_cost = PersonalizationCost.new
+             personalization_cost.cost = luxire_line_item[:total_personalization_cost]
+             personalization_cost.line_item_id = line_item.id
+             personalization_cost.save!
+            #  Fetch the order from line_item and update it
+            order = line_item.order
+            order.update!
+            #  Create the adjustment and update the order details
+             Spree::Adjustment.create!(
+                   amount: personalization_cost.cost,
+                   order: order,
+                   adjustable: line_item,
+                   source: personalization_cost,
+                   mandatory: true,
+                   label: "#{Spree.t(:personalization_cost)}"
+                 )
+             # Call line_item.save! method so that it can recalculate the adjustments
+             # And then update the order so that the adjustments are reflected to order
+             line_item.save!
+             order.update!
+           end
+        end
       end
      line_item
    end
