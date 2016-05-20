5e2a0a477bc81a887239a6a019cd34b419d2585f
Manish <manish@azureiken.com>
2016-05-20 20:38:49 +0530

    Added route for change_order_status method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  put 'api/change_order_status', to: 'custom_orders#change_order_status', defaults: {format: :json}
   get '/api/admin/products' => 'spree/api/products#admin_index'
   get '/api/admin/products/:id' => 'spree/api/products#admin_show'
 
