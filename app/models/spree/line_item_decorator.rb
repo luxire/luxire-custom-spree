e708a5bbb710b0dc592023c56526ecff228c0ad3
Manish <manish@azureiken.com>
2016-05-20 19:43:26 +0530

    Added a call to Recalculate adjustment

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,4 +1,11 @@
 Spree::LineItem.class_eval do
 has_one :luxire_line_item, class_name: 'LuxireLineItem'
 # accepts_nested_attributes_for :luxire_line_item
+
+def update_adjustments
+  # byebug
+    recalculate_adjustments
+    update_tax_charge # Called to ensure pre_tax_amount is updated.
+end
+
 end
