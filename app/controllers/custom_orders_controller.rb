a8279df9e2b52294d9c759ec7227f8eb2a88c0e0
Manish <manish@azureiken.com>
2016-06-16 15:37:41 +0530

    Added a new method change order status which will be used to change the state of the order

--- app/controllers/custom_orders_controller.rb
+++ app/controllers/custom_orders_controller.rb
@@ -2,14 +2,16 @@ class CustomOrdersController < Spree::Api::BaseController
 
   def get_order
       spree_api_token = request.headers["X-Spree-Token"] || params[:token]
-       if spree_api_token.nil?
+       if spree_api_token.nil? || spree_api_token.empty?
          guest_token = cookies.signed[:guest_token]
          @order = Spree::Order.where(guest_token: guest_token).where(completed_at: nil).where(user_id: nil).last
        else
          user = Spree::User.find_by_spree_api_key(spree_api_token)
-         @previous_order = Spree::Order.where(user_id: user.id).where(completed_at: nil).offset(1).last
-         @order = Spree::Order.where(user_id: user.id).where(completed_at: nil).last
-          unless(@previous_order.nil? || @order.nil?)
+	#byebug
+	@previous_order = Spree::Order.where(user_id: user.id).where(completed_at: nil).offset(1).last
+        @order = Spree::Order.where(user_id: user.id).where(completed_at: nil).last
+
+          unless(@previous_order.nil? || @order.nil? || @order.state != "cart")
   	        @previous_order.line_items.each do |line_item|
               line_item.order_id = @order.id
               line_item.save!
@@ -35,9 +37,44 @@ class CustomOrdersController < Spree::Api::BaseController
 
 
   def change_order_status
+    states = ["Order sheet generated", "Processing", "Shipped", "Delivered"]
     @order = Spree::Order.find(params[:order][:id])
     @luxire_order = @order.luxire_order
-    @luxire_order.fulfillment_status = params[:order][:status]
+    unless(states.include? params[:order][:status])
+      response= {msg: "Unknown status"}
+      render json: response.to_json, status: "422"
+      return;
+    end
+    current_status = @luxire_order.fulfillment_status
+    requested_status = params[:order][:status]
+    if current_status.nil?
+      if requested_status== "Order sheet generated"
+        @luxire_order.fulfillment_status = requested_status
+	@luxire_order.status_changed_time = Time.current
+      else
+        response= {msg: "Can't go to #{requested_status} directly"}
+        render json: response.to_json, status: "422"
+	return
+      end
+    else
+      if(states.find_index(requested_status) == (states.find_index(current_status) + 1))
+        @luxire_order.fulfillment_status = requested_status
+	@luxire_order.status_changed_time = Time.current
+      elsif(states.find_index(requested_status) == states.find_index(current_status))
+        response= {msg: "Current and requested status is same"}
+        render json: response.to_json, status: "422"
+        return
+      elsif (states.find_index(requested_status) < states.find_index(current_status))
+        response= {msg: "Can't go to #{requested_status} from #{current_status} state"}
+        render json: response.to_json, status: "422"
+        return
+      elsif (states.find_index(requested_status) > (states.find_index(current_status) + 1))
+        response= {msg: "Can't go to #{requested_status} directly from #{current_status} state"}
+        render json: response.to_json, status: "422"
+	return
+      end
+    end
+
     if @luxire_order.save
       response= {msg: "Order status changed successfully"}
       render json: response.to_json, status: "200"
