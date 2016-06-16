5fcce3aaaec82a9e850e7264cb9a50e2d6b64e7f
Manish <manish@azureiken.com>
2016-06-16 19:08:30 +0530

    creating the reset password link dynamically

--- app/mailers/luxire_user_mailer.rb
+++ app/mailers/luxire_user_mailer.rb
@@ -6,9 +6,10 @@ class LuxireUserMailer < ApplicationMailer
   #
   #   en.luxire_user_mailer.password_reset.subject
   #
-  def password_reset(user, raw_token)
+  def password_reset(user, raw_token, request)
     @user = user
     @user_raw_token = raw_token
+    @domain = request.domain || request.remote_ip
     mail :to =>user.email, :subject =>"Password Reset"
   end
 
