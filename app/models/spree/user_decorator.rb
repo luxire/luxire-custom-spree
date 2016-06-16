f1b22306e48d3fbfd3e4ebcd0d530640f900e17e
Manish <manish@azureiken.com>
2016-06-16 19:17:53 +0530

    Added User LuxireOrder association

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,4 +1,5 @@
 Spree::User.class_eval do
 has_one :luxire_customer, class_name: "LuxireCustomer"
 has_many :styles, class_name: "LuxireStyleMaster"
+has_many :luxire_orders, class_name: "LuxireOrder", through: :orders
 end
