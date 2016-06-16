598278d74fb6b0382c54c99dd2c3ab5809363e67
Manish <manish@azureiken.com>
2016-06-16 19:12:00 +0530

    Removed delete and destroyed functionalities from this application

--- app/models/mail_template.rb
+++ app/models/mail_template.rb
@@ -1,2 +1,21 @@
 class MailTemplate < ActiveRecord::Base
+  before_destroy :unauthorized_destroy
+  before_create :unauthorized_create
+
+  def delete
+    raise Exception, "You are not authorized to delete this record"
+  end
+
+  def self.delete_all
+    raise Exception, "You are not authorized to delete all record"
+  end
+
+  def unauthorized_destroy
+    raise Exception, "You are not authorized to destroy this record"
+  end
+
+  def unauthorized_create
+    raise Exception, "You are not authorized to create new record"
+  end
+
 end
