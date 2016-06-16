d41df3866a83720b5b7b25c30d243d2d3a04320c
Manish <manish@azureiken.com>
2016-06-16 19:15:15 +0530

    Changed the product style size from '240x240>' to '480X480#'

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,8 +1,9 @@
 Spree::Image.class_eval do
        has_attached_file :attachment,
-                     styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '500x500!>' },
+	styles: { mini: '100x100>', small: '240x240>', product: '480X480#', large: '' },
                      default_style: :product,
                      url: '/spree/products/:id/:style/:basename.:extension',
                      path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
-                     convert_options: { all: '-strip -auto-orient -colorspace sRGB -depth 300' }
+                     convert_options: { all: '-strip -auto-orient -colorspace sRGB -depth 300', large: '-gravity center -resize 40% -crop "600X600+0+0"'}
+# large: " -gravity center -resize '346.906x346.906+0+0'", product: " -gravity southeast" }
 end
