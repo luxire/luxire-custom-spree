d183c209ec4da339b679d166700a47d4ad4523d0
Manish <manish@azureiken.com>
2016-07-12 16:25:17 +0530

    Added a validation for position

--- app/models/product_measurement_type.rb
+++ app/models/product_measurement_type.rb
@@ -1,4 +1,6 @@
 class ProductMeasurementType < ActiveRecord::Base
   belongs_to :luxire_product_type, class_name: "LuxireProductType"
   belongs_to :measurement_type, class_name: "MeasurementType"
+  validates :position, presence: true
+  validates_uniqueness_of :position, :scope => :luxire_product_type_id
 end
