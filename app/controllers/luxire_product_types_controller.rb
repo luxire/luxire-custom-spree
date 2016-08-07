60fc53a0b846b154c835622122ad0c660c6ad58f
Manish <manish@azureiken.com>
2016-08-07 15:27:49 +0530

    Added two more service create_with_position and update_with_position as per requirement

--- app/controllers/luxire_product_types_controller.rb
+++ app/controllers/luxire_product_types_controller.rb
@@ -1,8 +1,8 @@
 class LuxireProductTypesController < Spree::Api::BaseController
   wrap_parameters format: [:json, :xml, :url_encoded_form, :multipart_form]
-  wrap_parameters :luxire_product_type, include: [:id, :product_type, :description, :measurement_type_ids, :image]
+  wrap_parameters :luxire_product_type, include: [:id, :product_type, :description, :measurement_type_ids, :image, :length_required]
 
-  before_action :set_luxire_product_type, only: [:show, :edit, :update, :destroy]
+  before_action :set_luxire_product_type, only: [:show, :edit, :update, :destroy, :update_with_position]
   require 'ostruct'
 
   # GET /luxire_product_types
@@ -97,6 +97,82 @@ class LuxireProductTypesController < Spree::Api::BaseController
     end
   end
 
+  # POST /luxire_product_types_with_position
+  # POST /luxire_product_types_with_position.json
+  def create_with_position
+    @luxire_product_type = LuxireProductType.new(luxire_product_type_params)
+    error_message = nil
+    begin
+      LuxireProductType.transaction do
+           @luxire_product_type.save!
+
+           if(params[:luxire_product_type][:measurement_type_ids])
+            if(params[:luxire_product_type][:measurement_type_ids].length>0)
+              @measurement_type_ids = params[:luxire_product_type][:measurement_type_ids]
+              populate_product_measurement_type("customization")
+              populate_product_measurement_type("personalization")
+              populate_product_measurement_type("body_measurement")
+              populate_product_measurement_type("standard_measurement")
+            end
+          end
+        end
+      rescue Exception => e
+        error_message = e.message
+        logger.error("Erro while populating Product type record and the reason is #{error_message}")
+      end
+
+    respond_to do |format|
+      unless error_message
+        format.html { redirect_to @luxire_product_type, notice: 'Luxire product type was successfully created.' }
+        format.json { render :show, status: :created, location: @luxire_product_type }
+      else
+        format.html { render :new }
+        format.json do
+          msg = {msg: error_message}
+          render json: msg.to_json, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+
+    # PATCH/PUT /luxire_product_types_with_position/1
+    # PATCH/PUT /luxire_product_types_with_position/1.json
+    def update_with_position
+      error_message = nil
+      begin
+        LuxireProductType.transaction do
+             @luxire_product_type.update!(luxire_product_type_params)
+
+             if(params[:luxire_product_type][:measurement_type_ids])
+              if(params[:luxire_product_type][:measurement_type_ids].length>0)
+                @measurement_type_ids = params[:luxire_product_type][:measurement_type_ids]
+                populate_product_measurement_type("customization")
+                populate_product_measurement_type("personalization")
+                populate_product_measurement_type("body_measurement")
+                populate_product_measurement_type("standard_measurement")
+              end
+            end
+          end
+        rescue Exception => e
+          error_message = e.message
+          logger.error("Erro while populating Product type record and the reason is #{error_message}")
+        end
+
+      respond_to do |format|
+        if error_message.nil?
+          format.html { redirect_to @luxire_product_type, notice: 'Luxire product type was successfully updated.' }
+          format.json { render :show, status: :ok, location: @luxire_product_type }
+        else
+          format.html { render :edit }
+          format.json {
+            msg = {msg: error_message}
+            render json: msg.to_json, status: :unprocessable_entity
+          }
+        end
+      end
+    end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_luxire_product_type
@@ -105,6 +181,18 @@ class LuxireProductTypesController < Spree::Api::BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def luxire_product_type_params
-      params.require(:luxire_product_type).permit(:product_type, :description, :measurement_type_ids, :image)
+      params.require(:luxire_product_type).permit(:product_type, :description, :measurement_type_ids, :image, :length_required)
+    end
+
+    def populate_product_measurement_type(measurement_type)
+      return if @measurement_type_ids[measurement_type].nil?
+      @measurement_type_ids[measurement_type].keys.each do |key|
+        product_measurement_type = ProductMeasurementType.where(luxire_product_type_id: @luxire_product_type.id, measurement_type_id: key).take
+        unless product_measurement_type
+          ProductMeasurementType.create!({luxire_product_type_id: @luxire_product_type.id, position: @measurement_type_ids[measurement_type][key], measurement_type_id: key});
+        else
+          product_measurement_type.update!({luxire_product_type_id: @luxire_product_type.id, position: @measurement_type_ids[measurement_type][key], measurement_type_id: key});
+        end
+      end
     end
 end
