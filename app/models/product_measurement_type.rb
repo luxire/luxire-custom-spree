6f5c9c79def615369ffe8fbc00943a3427f20102
Manish <manish@azureiken.com>
2016-08-07 15:35:05 +0530

    Added a validation on position

--- app/models/product_measurement_type.rb
+++ app/models/product_measurement_type.rb
@@ -1,6 +1,21 @@
 class ProductMeasurementType < ActiveRecord::Base
   belongs_to :luxire_product_type, class_name: "LuxireProductType"
   belongs_to :measurement_type, class_name: "MeasurementType"
-  validates :position, presence: true
-  validates_uniqueness_of :position, :scope => :luxire_product_type_id
+  validates_presence_of :measurement_type_id, :luxire_product_type_id, :position
+
+  validate :uniqueness_of_position
+
+  def uniqueness_of_position
+    if position && measurement_type_id && luxire_product_type_id
+      measurement_type = MeasurementType.find(measurement_type_id)
+      pmt = ProductMeasurementType.joins(:measurement_type).where("measurement_types.category = ? AND measurement_types.sub_category = ? AND product_measurement_types.position = ? AND product_measurement_types.luxire_product_type_id = ?", measurement_type.category, measurement_type.sub_category, position, luxire_product_type_id).take
+      unless pmt.nil?
+        unless measurement_type.sub_category.nil? || measurement_type.sub_category.blank?
+          errors.add(:position, "Duplicate position for #{measurement_type.category} category and #{measurement_type.sub_category} sub_category of measurement_type id #{measurement_type.id}.")
+        else
+          errors.add(:position, "Duplicate position for #{measurement_type.category} category")
+        end
+      end
+    end
+  end
 end
