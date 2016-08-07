ad8754de1fa6a04b3e88a4138d2fe74957f748f5
Manish <manish@azureiken.com>
2016-08-07 15:30:27 +0530

    Added two helpers to format the json object

--- app/helpers/luxire_style_masters_helper.rb
+++ app/helpers/luxire_style_masters_helper.rb
@@ -1,2 +1,18 @@
 module LuxireStyleMastersHelper
+  def real_images(luxire_style_master)
+     get_images("real",luxire_style_master)
+  end
+
+  def sketch_images(luxire_style_master)
+    get_images("sketch",luxire_style_master)
+  end
+
+  def get_images(image_type, luxire_style_master)
+    luxire_style_master_images = LuxireStyleMasterImage.where("lower(category) =? and luxire_style_master_id =?", image_type, luxire_style_master.id);
+    real_images = []
+    luxire_style_master_images.each do |luxire_style_master_img|
+      real_images << {small: luxire_style_master_img.image.url(:small), medium: luxire_style_master_img.image.url(:medium), large: luxire_style_master_img.image.url(:large), alternate_text: luxire_style_master_img.alternate_text, category: luxire_style_master_img.category}
+    end
+    real_images
+  end
 end
