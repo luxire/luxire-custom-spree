aec08e826b9b7b8666200c52c7d9f684097f4db0
Manish <manish@azureiken.com>
2016-08-17 19:20:52 +0530

    Added dependent destroy for luxire taxonomy relation

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,3 +1,3 @@
 Spree::Taxonomy.class_eval do
-has_one :luxire_taxonomy, class_name: "LuxireTaxonomy", foreign_key: :spree_taxonomy_id
+has_one :luxire_taxonomy, class_name: "LuxireTaxonomy", foreign_key: :spree_taxonomy_id, dependent: :destroy
 end
