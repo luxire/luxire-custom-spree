55ad3b5e8c341f80b7c6074fee4f47fd2b94207b
Manish <manish@azureiken.com>
2016-08-17 19:21:43 +0530

    Added dependent destroy for luxire customer relation

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,5 @@
 Spree::User.class_eval do
-has_one :luxire_customer, class_name: "LuxireCustomer"
+has_one :luxire_customer, class_name: "LuxireCustomer", dependent: :destroy
 has_many :styles, class_name: "LuxireStyleMaster"
 has_many :luxire_orders, class_name: "LuxireOrder", through: :orders
 end
