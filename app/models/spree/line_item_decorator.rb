9610a0f99f715006f0335ec050a5e35ff354666a
Manish <manish@azureiken.com>
2016-08-17 19:18:11 +0530

    Added dependent destroy for luxirelineitem relation

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,5 +1,6 @@
 Spree::LineItem.class_eval do
-has_one :luxire_line_item, class_name: 'LuxireLineItem'
+has_one :luxire_line_item, class_name: 'LuxireLineItem', dependent: :destroy
+has_one :luxire_stock, class_name: 'LuxireStock', through: :product
 # accepts_nested_attributes_for :luxire_line_item
 
 def update_adjustments
