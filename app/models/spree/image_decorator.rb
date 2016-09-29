bd5f4c1d415c18c8e7def429dee2b7da3685e2b7
Manish <manish@azureiken.com>
2016-09-29 14:59:49 +0530

    Changed the large style to 900X

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,9 +1,9 @@
 Spree::Image.class_eval do
        has_attached_file :attachment,
-	styles: { mini: '100x100>', small: '240x240>', product: '540X540', large: '' },
+	styles: { mini: '100x100>', small: '240x240>', product: '540X540', large: '900X' },
                      default_style: :product,
                      url: '/spree/products/:id/:style/:basename.:extension',
                      path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
-                     convert_options: { all: '-strip -auto-orient -colorspace sRGB -depth 300', large: '-gravity center -resize 40% -crop "600X600+0+0"'}
+                     convert_options: { all: '-strip -auto-orient -colorspace sRGB -depth 300'}
 # large: " -gravity center -resize '346.906x346.906+0+0'", product: " -gravity southeast" }
 end
