2de4ee395b9c59498ebdeada6edb0a44a275dd01
Manish <manish@azureiken.com>
2016-10-21 01:04:48 +0530

    Added position to LuxireStyleMaster

--- db/migrate/20161012171923_add_column_position_to_luxire_product_type_style_master.rb
+++ db/migrate/20161012171923_add_column_position_to_luxire_product_type_style_master.rb
@@ -1,5 +1,5 @@
 class AddColumnPositionToLuxireProductTypeStyleMaster < ActiveRecord::Migration
   def change
-    add_column :luxire_product_type_style_masters, :position, :integer
+    add_column :luxire_style_masters, :position, :integer
   end
 end
