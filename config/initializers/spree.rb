1fd98691b51ad24c09608ab468c7a57495ce50ed
Ubuntu <luxire@cloudhop-store-lux-stage.mrmphw5fh4outbzbdejx53rcdd.ix.internal.cloudapp.net>
2016-10-25 10:25:26 +0000

    Removed S3 configuration

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -19,34 +19,4 @@ Spree.user_class = "Spree::User"
 Rails.application.config.spree.payment_methods << Spree::Gateway::Ebsin
 
 Spree::Api::Config[:requires_authentication] = false
-attachment_config = {
-
-  s3_credentials: {
-    access_key_id:     'AKIAJZIAAC4BYQSJTBZQ',
-    secret_access_key: '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',
-    bucket:            'atspree-dev'
-  },
-
-  storage:        :s3,
-  s3_headers:     { "Cache-Control" => "max-age=31557600" },
-  s3_protocol:    "https",
-  bucket:         'atspree-dev',
-  url:            ":s3_domain_url",
-
-  styles: {
-      mini:     "48x48>",
-      small:    "100x100>",
-      product:  "240x240>",
-      large:    "600x600>"
-  },
-
-  path:           "/:class/:id/:style/:basename.:extension",
-  default_url:    "/:class/:id/:style/:basename.:extension",
-  default_style:  "product"
-}
-
-attachment_config.each do |key, value|
-  Rails.logger.debug "#{key}: #{value}"
-  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-end
 Spree::PermittedAttributes.product_attributes << :is_gift_card
