2435ba828b36e8b61709133e3ee8f9ba68d5ec2d
Ubuntu <luxire@cloudhop-store-lux-stage.mrmphw5fh4outbzbdejx53rcdd.ix.internal.cloudapp.net>
2016-10-25 10:18:16 +0000

    Changed the return URL

--- app/controllers/spree/paypal_controller_decorator.rb
+++ app/controllers/spree/paypal_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::PaypalController.class_eval do
           :InvoiceID => order.number,
           :BuyerEmail => order.email,
           :ReturnURL => confirm_paypal_url(:payment_method_id => params[:payment_method_id], :utm_nooverride => 1),
-          :CancelURL =>  "http://104.215.252.45/#/cart",
+          :CancelURL =>  "http://beta.luxire.com/#/checkout/payment",
           :SolutionType => payment_method.preferred_solution.present? ? payment_method.preferred_solution : "Mark",
           :LandingPage => payment_method.preferred_landing_page.present? ? payment_method.preferred_landing_page : "Billing",
           :cppheaderimage => payment_method.preferred_logourl.present? ? payment_method.preferred_logourl : "",
