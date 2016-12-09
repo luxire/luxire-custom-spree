79e3cbc3b1de3ab5179ea7f09831b1882dd9f9a3
Ubuntu <luxire@cloudhop-store-lux-stage.mrmphw5fh4outbzbdejx53rcdd.ix.internal.cloudapp.net>
2016-12-09 10:41:51 +0000

    Increased the precision of cost field

--- db/migrate/20160426120654_create_personalization_costs.rb
+++ db/migrate/20160426120654_create_personalization_costs.rb
@@ -1,7 +1,7 @@
 class CreatePersonalizationCosts < ActiveRecord::Migration
   def change
     create_table :personalization_costs do |t|
-      t.decimal :cost, precision: 5, scale: 2
+      t.decimal :cost, precision: 10, scale: 2
       t.integer :line_item_id
 
       t.timestamps null: false
