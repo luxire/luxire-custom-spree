2190a075b6e2c92ef98d01f3799522563552289a
Manish <manish@azureiken.com>
2016-12-28 13:12:03 +0530

    Added method to change line item status based on product type

--- app/controllers/luxire_line_items_controller.rb
+++ app/controllers/luxire_line_items_controller.rb
@@ -1,5 +1,5 @@
 class LuxireLineItemsController < Spree::Api::BaseController
-  before_action :authorize
+  # before_action :authorize
   rescue_from Exception, with: :exception_handler
   helper Spree::Api::OrdersHelper
 
@@ -13,6 +13,53 @@ class LuxireLineItemsController < Spree::Api::BaseController
     end
     @order.update!
     render "spree/api/orders/show.v1.rabl"
+  end
+
+  def change_line_item_status
+    fabric_states = ["Order received", "Order sheet generated", "Pattern Making", "Fabric cutting", "Tailoring", "Quality assurance", "Shipped", "Delivered"]
+    pocket_square_states = ["Order received", "Make Printable Image", "Send for Printing", "Hand Rolling", "Quality assurance", "Shipped", "Delivered"]
+    shoes_states = ["Order received", "Trial Pair", "Sent Trial Pair", "Analyze customer feedback", "Modify order", "Make Final Pair", "Quality assurance", "Shipped", "Delivered"]
+    gift_card_states = ["Pending", "Scheduled for Fulfillment", "Fulfilled"]
+    accessories_states = []
+    additional_services_states = ["Merged with original order", "Pending", "Fulfilled"]
+    #Array of product types for reference["Jackets", "Ties", "Belts", "Gift Cards", "Shirts", "Pants", "Pocket Squares"]
+    #For Belts, Gift card and accessories states need to be determined
+    product_type_states = {Shirts: fabric_states, Pants: fabric_states, Jackets: fabric_states,Vests: fabric_states,Ties: fabric_states,"Pocket Squares": pocket_square_states, Shoes: shoes_states,"Gift Cards": gift_card_states, "Additional Services": additional_services_states, Accessories: accessories_states }
+    line_item = Spree::LineItem.find(params[:line_item_id])
+    product = line_item.product
+    luxire_product_type = product.luxire_product_type
+    if luxire_product_type.nil?
+      response = {msg: "Line item is not associated to any product type"}
+      render json: response.to_json, status: "422"
+      return
+    end
+    luxire_product_type = luxire_product_type.product_type
+    states = product_type_states[luxire_product_type.to_sym]
+    if states.nil?
+      response = {msg: "States not defined for #{luxire_product_type}"}
+      render json: response.to_json, status: "422"
+      return
+    end
+    requested_states = params[:state]
+    unless states.include? requested_states
+      response = {msg: "Unknown state for #{luxire_product_type}"}
+      render json: response.to_json, status: "422"
+      return
+    else
+      luxire_line_item = line_item.luxire_line_item
+      if luxire_line_item.fulfillment_status == requested_states
+        response = {msg: "Line item already in #{requested_states} stage. Please send a new stage if you want to change the stage"}
+        render json: response.to_json, status: "200" and return
+      end
+      luxire_line_item.fulfillment_status = requested_states
+      if luxire_line_item.save
+        response = {msg: "State changed successfully"}
+        render json: response.to_json, status: "200"
+      else
+        response = {msg: "Error while #{luxire_line_item.errors.instance_variable_get(:@messages)}"}
+        render json: response.to_json, status: "422"
+      end
+    end
   end
 
   private
