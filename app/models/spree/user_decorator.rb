37ac78e07ad244f96cb68138efbeed4ec103c2b3
Manish <manish@azureiken.com>
2017-01-09 15:11:36 +0530

    Added a relation has many unique address for user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,4 +2,5 @@ Spree::User.class_eval do
 has_one :luxire_customer, class_name: "LuxireCustomer", dependent: :destroy
 has_many :styles, class_name: "LuxireStyleMaster"
 has_many :luxire_orders, class_name: "LuxireOrder", through: :orders
+has_many :unique_addresses, -> { where(deleted_at: nil).select("DISTINCT ON (firstname, lastname, address1, address2, city, zipcode, state_name, state_id, country_id) *")  }, class_name: Spree::Address
 end
