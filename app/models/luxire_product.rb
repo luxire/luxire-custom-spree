8e9e8cbdda7906356adefa944cedfbcc738a2a5d
manish@azureiken.com <luxire@cloudhop-store-lux-stage.mrmphw5fh4outbzbdejx53rcdd.ix.internal.cloudapp.net>
2017-05-04 13:03:27 +0000

    Touch product when luxire_product is updated

--- app/models/luxire_product.rb
+++ app/models/luxire_product.rb
@@ -10,7 +10,12 @@ class LuxireProduct < ActiveRecord::Base
 
   accepts_nested_attributes_for :luxire_stock
   validates :length_required, presence: true,numericality: true
+  after_save :touch_product
   # validates_presence_of (LuxireProduct.column_names - ["id","created_at","updated_at", "deleted_at", "gift_card_flag", "luxire_product_type_id"]), :if => lambda { |product_type| product_type.req_validation.eql? true }
   # validates_inclusion_of :gift_card_flag, in: [true, false], :if => lambda { |product_type| product_type.req_validation.eql? true }
   # validates_presence_of :luxire_product_type_id
+
+  def touch_product
+    self.product.touch
+  end
 end
