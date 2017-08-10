835dac022f731e94e75495b4871d38aaf7714633
Mudassir <mudassir@azureiken.com>
2017-08-10 19:44:09 +0530

    Send mail even if there is no buggy product

--- app/mailers/product_upload_mailer.rb
+++ app/mailers/product_upload_mailer.rb
@@ -1,10 +1,13 @@
 class ProductUploadMailer < ApplicationMailer
-  def product_upload_status(buggy_record, count, attachment_path)
-    attachments['error.xlsx'] = File.read(attachment_path)
-    @uploaded_product = count - buggy_record.length
-    mail(:to => Spree.admin_email,
-         :subject =>"Product upload status #{Time.current}"
-        )
-  end
+	def product_upload_status(buggy_record, count, attachment_path)
+		if attachment_path
+			attachments['error.xlsx'] = File.read(attachment_path)
+		end
+		@buggy_record = buggy_record
+		@uploaded_product = count - buggy_record.length
+		mail(:to => Spree.admin_email,
+			:subject =>"Product upload status #{Time.current}"
+		)
+	end
 
 end
